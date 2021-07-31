c5fd8a03e05dba4d7bbe3ac6628b6c989ba894a7
nimamt <mtaghavinima@gmail.com>
2021-08-01 02:55:31 +0430

    Created intro.

---
+++ src/Pages/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="home">
+            <section className="home__intro">
+                <img src="./images" alt="" className="home__introImg" />
+            </section>
+        </div>
+    )
+}
